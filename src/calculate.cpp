7d97b648c267079b53703ce0e761bc89c4321181
Vladimir Kormakov <v.kormakov@skoltech.ru>
2024-07-08 18:47:09 +0300

    feat: check simple scalar

--- src/calculate.cpp
+++ src/calculate.cpp
@@ -4,6 +4,24 @@
 #include <starpu.h>
 #include "nntile_logger.hpp"
 
+
+
+
+void acquire_callback(void *arg)
+{
+    starpu_data_handle_t *sum_handle = (starpu_data_handle_t *)arg;
+    float *sum_ptr = (float *)starpu_data_get_local_ptr(*sum_handle);
+
+    starpu_data_release(*sum_handle);
+
+    char message[256];
+    snprintf(message, sizeof(message), "{\"sum\": \"%f\"}\n", *sum_ptr);
+    std::cout << "DATA : " << message << std::endl;
+
+    delete sum_handle;
+}
+
+
 void rand_time_cpu(void *buffers[], void *cl_arg)
 {
   struct starpu_vector_interface *sum_iface = (struct starpu_vector_interface *)buffers[0];
@@ -37,12 +55,12 @@ int main()
 {
   int ret = starpu_init(NULL);
   STARPU_CHECK_RETURN_VALUE(ret, "starpu_init");
-  nntile_logger_init();
+  // nntile_logger_init();
   float sum[1] = {0};
   starpu_data_handle_t sum_handle;
   starpu_vector_data_register(&sum_handle, STARPU_MAIN_RAM, (uintptr_t)sum, 1, sizeof(sum[0]));
   float from = 0.1, to = 0.3;
-  int N = 50;
+  int N = 100;
   for (int i = 0; i < N; i++)
   {
     ret = starpu_task_insert(&rand_time_cl, STARPU_VALUE, &from, sizeof(from),
@@ -51,15 +69,18 @@ int main()
                              STARPU_FLOPS, (double)150.111,
                              0);
     STARPU_CHECK_RETURN_VALUE(ret, "starpu_task_insert");
+    starpu_data_handle_t *sum_handle_copy = new starpu_data_handle_t;
+    *sum_handle_copy = sum_handle;
+    starpu_data_acquire_cb(sum_handle, STARPU_R, acquire_callback, sum_handle_copy);
   }
   starpu_task_wait_for_all();
   starpu_data_unregister(sum_handle);
-  std::cout << "N = " << N << std::endl;
-  std::cout << "from = " << from << std::endl;
-  std::cout << "to = " << to << std::endl;
-  std::cout << "sum = " << sum[0] << std::endl;
+  // std::cout << "N = " << N << std::endl;
+  // std::cout << "from = " << from << std::endl;
+  // std::cout << "to = " << to << std::endl;
+  // std::cout << "sum = " << sum[0] << std::endl;
 
-  nntile_logger_shutdown();
+  // nntile_logger_shutdown();
   starpu_shutdown();
   return 0;
 }
